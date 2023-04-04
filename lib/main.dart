a4eed44ccd6f0297fd96ef52d01ee3b38464acd6
himanshu21072002 <himanshu.gangwar2000@gmail.com>
2023-04-04 09:26:10 +0530

    replaced MaterialApp with CupetinoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gsoc2/spash_screen.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       home:MySplashScreen(),
     );
